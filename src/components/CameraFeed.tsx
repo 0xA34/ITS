623593fe8e30c7ffbd417015606cdbedb4ba2425
Nguyễn Văn Minh Giang <99959855+NguyenVanMinhGiang2005@users.noreply.github.com>
2025-12-19 12:32:25 +0700

    Refactor CameraFeed component to handle m3u8 URLs

--- src/components/CameraFeed.tsx
+++ src/components/CameraFeed.tsx
@@ -1,16 +1,67 @@
 // src\components\CameraFeed.tsx
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useMemo } from "react";
+import Hls from "hls.js" // hls 
 
 interface CameraFeedProps {
   cameraId: string;
   cameraName: string;
   url: string;
+   
 }
+
+// url_m3u8.replace(/[&?]videoUrl=.*/, "")
+// xử lí url + ảnh + video
+function xuli_m3u8(url_m3u8: string): string{
+  try {
+    // url
+    const u = new URL(url_m3u8);
+    if (u.href.includes("videoUrl")){
+      console.log(url_m3u8.replace(/[&?]videoUrl=.*/, ""));
+      return url_m3u8.replace(/[&?]videoUrl=.*/, "")
+    } else {
+      return null;
+    }
+  } catch {
+    // path video + anh
+    console.log('2'); 
+  }
+}
+
 
 const CameraFeed = ({ cameraId, cameraName, url }: CameraFeedProps) => {
   const x = false
+  
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const m3u8Url = useMemo(() => xuli_m3u8(url), [url]);
 
-  // Thời gian 
+  const isHls = !!m3u8Url;
+  console.log(isHls)
+  useEffect(() => {
+    if (!isHls || !m3u8Url || !videoRef.current) return;
+
+    const video = videoRef.current;
+
+    // Safari / iOS có thể play native
+    if (video.canPlayType("application/vnd.apple.mpegurl")) {
+      video.src = m3u8Url;
+      return;
+    }
+
+    // Chrome/Edge/Firefox: dùng hls.js
+    if (Hls.isSupported()) {
+      const hls = new Hls({
+        lowLatencyMode: true,
+      });
+      hls.loadSource(m3u8Url);
+      hls.attachMedia(video);
+
+      return () => {
+        hls.destroy();
+      };
+    }
+  }, [isHls, m3u8Url]);
+
+  // Thời gian
   const [now, setNow] = useState(() => new Date());
   useEffect(() => {
     const id = setInterval(() => setNow(new Date()), 1000); // cập nhật mỗi giây
@@ -44,7 +95,7 @@ const CameraFeed = ({ cameraId, cameraName, url }: CameraFeedProps) => {
     >
       {/* Camera View */}
       <div className="aspect-video bg-gradient-to-br from-secondary to-camera-bg flex items-center justify-center relative group">
-        <img src = {src} className="w-full h-full object-cover"/>
+        {!isHls ? (<img src={url} className="h-full w-full object-cover" />): (<iframe src={url} className="h-full w-full object-cover"/>)}
       </div>
 
       {/* thông tin IdCamera + cameraName */}
