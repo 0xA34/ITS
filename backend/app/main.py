2567321bb42ce1aeb0f451fde3eb441909c833c9
Nguyễn Văn Minh Giang <99959855+NguyenVanMinhGiang2005@users.noreply.github.com>
2025-12-25 23:07:38 +0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
-from app.core.middleware import RequestIdLoggingMiddleware
-from app.database.mongo import connect_mongo, close_mongo
 from app.routes.camera_routes import router as camera_router
 
 app = FastAPI()
 
-# CORS (dev)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
@@ -15,22 +12,11 @@ app.add_middleware(
         "http://127.0.0.1:8080",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        ],
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# Custom middleware
-app.add_middleware(RequestIdLoggingMiddleware)
-
-@app.on_event("startup")
-async def on_startup():
-    await connect_mongo()
-
-@app.on_event("shutdown")
-async def on_shutdown():
-    await close_mongo()
 
 @app.get("/api/health")
 def health():
